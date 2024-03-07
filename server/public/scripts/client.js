5907b1d877dd738b0e3d3ee928b9fc49ea6ecc81
Sarah McCartney <smmc16@gmail.com>
2024-03-07 00:04:21 -0800

    added to client side post request to create a calculation object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,8 @@ console.log('client.js is sourced!');
 function submitCalculations(event) {
     event.preventDefault();
 
-    getCalculations()
-    postCalculations()
+    getCalculations();
+    postCalculations();
 }
 
 let operatorObject = {
@@ -56,9 +56,28 @@ function getCalculations() {
 }
 
 function postCalculations() {
+    let numOne = document.querySelector('#numOne').value
+    let numTwo = document.querySelector('#numTwo').value
+
+    let calculationObject = {
+        numOne: numOne,
+        numTwo: numTwo,
+    }
+
+    if(operatorObject.add === true) {
+        calculationObject.operator = '+'
+    } else if(operatorObject.subtract === true) {
+        calculationObject.operator = '-'
+    } else if(operatorObject.multiply === true) {
+        calculationObject.operator = '*'
+    } else if(operatorObject.divide === true) {
+        calculationObject.operator = '/'
+    };
+    console.log(calculationObject);
+    
     axios.post('/calculations', calculationObject).then((response) => {
         console.log(response);
-        
+
     
     }).catch((error) => {
         console.log(error);
